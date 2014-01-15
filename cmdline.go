263e2cade22578dcd38ff051d345fd5bd72a3207
Julz Friedman <julz.friedman@uk.ibm.com>
2014-01-15 14:32:56 +0000

    Initial support for concurrent/serial load
    Only via command line, but with a nice animated terminal UI.

--- cmdline.go
+++ cmdline.go
@@ -1,7 +1,8 @@
 package pat
 
 import (
-	"github.com/julz/pat/benchmarker"
+	"fmt"
+	. "github.com/julz/pat/benchmarker"
 	"time"
 )
 
@@ -10,7 +11,27 @@ type Response struct {
 	Timestamp int64
 }
 
-func RunCommandLine() *Response {
-	totalTime := benchmarker.Time(push)
+func RunCommandLine(pushes int, concurrency int) *Response {
+	result := make(chan time.Duration)
+	go func(result chan time.Duration) {
+		var avg int64
+		var total int64
+		var n int64
+		for r := range result {
+			total = total + r.Nanoseconds()
+			n = n + 1
+			avg = total / n
+			fmt.Print("\033[2J\033[;H")
+			fmt.Println("Cloud Foundry Performance Acceptance Tests")
+			fmt.Printf("Test underway.  Pushes: \x1b[32m%v\x1b[0m  Concurrency: \x1b[32m%v\x1b[0m\n", pushes, concurrency)
+			fmt.Println("----------------------------------------------------------\n")
+			fmt.Printf("Total pushes:   \x1b[32m%v\x1b[0m\n", n)
+			fmt.Printf("Latest Push:    \x1b[32m%v\x1b[0m\n", r.Nanoseconds())
+			fmt.Printf("Average:        \x1b[32m%v\x1b[0m\n", avg)
+			fmt.Printf("Total time:     \x1b[32m%v\x1b[0m\n", total)
+		}
+	}(result)
+
+	totalTime := Time(func() { ExecuteConcurrently(concurrency, Repeat(pushes, Timed(result, dummy))) })
 	return &Response{totalTime.Nanoseconds(), time.Now().UnixNano()}
 }
--- pat/main.go
+++ pat/main.go
@@ -8,6 +8,8 @@ import (
 
 func main() {
 	server := flag.Bool("server", false, "true to run the HTTP server interface")
+	pushes := flag.Int("pushes", 1, "number of pushes to attempt")
+	concurrency := flag.Int("concurrency", 1, "max number of pushes to attempt in parallel")
 	flag.Parse()
 
 	if *server == true {
@@ -15,7 +17,7 @@ func main() {
 		pat.Serve()
 		pat.Bind()
 	} else {
-		resp := pat.RunCommandLine()
-		fmt.Printf("Total Time: %d", resp.TotalTime)
+		resp := pat.RunCommandLine(*pushes, *concurrency)
+		fmt.Printf("\n\nTest Complete. Total Time: %d", resp.TotalTime)
 	}
 }
