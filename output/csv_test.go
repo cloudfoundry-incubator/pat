21bde08a92e9ce157d09f87ef0c8a4da09304df7
Julz Friedman <julz.friedman@uk.ibm.com>
2014-01-19 00:26:59 +0000

    add output/*.go. Oops

---
+++ output/csv_test.go
@@ -0,0 +1,13 @@
+package output_test
+
+import (
+	. "github.com/julz/pat/output"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Csv", func() {
+	It("Converts a list of experiments to a CSV", func() {
+		
+	})
+})
