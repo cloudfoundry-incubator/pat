890d2f8fe3dd9b860bb0894d9a4abc7eb1b0e0cb
Julz Friedman <julz.friedman@uk.ibm.com>
2014-01-20 16:57:31 +0000

    Create csv output directory if neccessary

--- output/csv_test.go
+++ output/csv_test.go
@@ -2,9 +2,29 @@ package output_test
 
 import (
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/julz/pat/output"
+	"github.com/julz/pat/experiment"
+	"io/ioutil"
+	"strings"
 )
 
 var _ = Describe("Csv", func() {
-	PIt("Converts a list of experiments to a CSV", func() {
+	It("Converts a list of experiments to a CSV", func() {
+		filename := "/var/tmp/output/foo.csv"
+		out := NewCsvWriter(filename)
+		ch := make(chan *experiment.Sample)
+		go func() {
+			ch <- &experiment.Sample{}
+			ch <- &experiment.Sample{}
+			close(ch)
+		}()
+		out.Write(ch)
+
+		in, err := ioutil.ReadFile(filename)
+		Ω(err).ShouldNot(HaveOccurred())
+		Ω(strings.Split(string(in), "\n")[0]).Should(Equal("duration,wallTime,average,workers"))
+		Ω(strings.Split(string(in), "\n")[1]).Should(Equal("0,0,0,0"))
+		Ω(strings.Split(string(in), "\n")[2]).Should(Equal("0,0,0,0"))
 	})
 })
