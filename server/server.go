0b49f8f5191239c48d8cdfe28d10b274f1d331af
Julz Friedman <julz.friedman@uk.ibm.com>
2014-01-21 07:33:27 +0000

    objectify handler methods

--- server/server.go
+++ server/server.go
@@ -35,12 +35,12 @@ func Serve() {
 func ServeWithArgs(baseDir string, csvDir string) {
 	r := mux.NewRouter()
 	ctx := &context{r, baseDir, csvDir, make(map[string][]*experiment.Sample)}
-	r.Methods("GET").Path("/experiments/").HandlerFunc(handler(ctx, handleList))
-	r.Methods("GET").Path("/experiments/{name}").HandlerFunc(handler(ctx, handleGetExperiment)).Name("experiment")
-	r.Methods("POST").Path("/experiments/").HandlerFunc(handler(ctx, handlePush))
+	r.Methods("GET").Path("/experiments/").HandlerFunc(handler(ctx.handleList))
+	r.Methods("GET").Path("/experiments/{name}").HandlerFunc(handler(ctx.handleGetExperiment)).Name("experiment")
+	r.Methods("POST").Path("/experiments/").HandlerFunc(handler(ctx.handlePush))
 
 	// BUG(jz) For easy web-browser testing, remove
-	r.HandleFunc("/POST/experiments/", handler(ctx, handlePush))
+	r.HandleFunc("/POST/experiments/", handler(ctx.handlePush))
 
 	http.Handle("/ui/", http.StripPrefix("/ui/", http.FileServer(http.Dir("ui"))))
 	http.Handle("/csv/experiments/", http.StripPrefix("/csv/experiments/", http.FileServer(http.Dir(ctx.csvDir))))
@@ -61,7 +61,7 @@ type listResponse struct {
 	Items interface{}
 }
 
-func handleList(ctx *context, w http.ResponseWriter, r *http.Request) (interface{}, error) {
+func (ctx *context) handleList(w http.ResponseWriter, r *http.Request) (interface{}, error) {
 	from, err := strconv.Atoi(r.FormValue("from"))
 	to, err := strconv.Atoi(r.FormValue("to"))
 	if err == nil {
@@ -73,7 +73,7 @@ func handleList(ctx *context, w http.ResponseWriter, r *http.Request) (interface
 	}
 }
 
-func handlePush(ctx *context, w http.ResponseWriter, r *http.Request) (interface{}, error) {
+func (ctx *context) handlePush(w http.ResponseWriter, r *http.Request) (interface{}, error) {
 	fmt.Println("handlepush")
 	name, _ := uuid.NewV4()
 
@@ -98,26 +98,26 @@ func handlePush(ctx *context, w http.ResponseWriter, r *http.Request) (interface
 	return ctx.router.Get("experiment").URL("name", name.String())
 }
 
-func handleGetExperiment(ctx *context, w http.ResponseWriter, r *http.Request) (interface{}, error) {
+func (ctx *context) handleGetExperiment(w http.ResponseWriter, r *http.Request) (interface{}, error) {
 	name := mux.Vars(r)["name"]
 	// TODO(jz) only send back since N
 	return &listResponse{ctx.running[name]}, nil
 }
 
-func (context *context) buffer(name string, samples chan *experiment.Sample) {
+func (ctx *context) buffer(name string, samples chan *experiment.Sample) {
 	for s := range samples {
 		// FIXME(jz) - need to clear this at some point, memory leak..
-		context.running[name] = append(context.running[name], s)
+		ctx.running[name] = append(ctx.running[name], s)
 	}
 }
 
-func handler(ctx *context, fn func(ctx *context, w http.ResponseWriter, r *http.Request) (interface{}, error)) http.HandlerFunc {
+func handler(fn func(w http.ResponseWriter, r *http.Request) (interface{}, error)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var err error
 		var response interface{}
 		var encoded []byte
 
-		if response, err = fn(ctx, w, r); err == nil {
+		if response, err = fn(w, r); err == nil {
 			switch r := response.(type) {
 			case *url.URL:
 				w.Header().Set("Location", r.String())
