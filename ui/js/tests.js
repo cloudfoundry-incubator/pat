92463af4e35e872846b37c1fd0651425704f5c5b
Julz Friedman <julz.friedman@uk.ibm.com>
2014-01-25 12:44:46 +0000

    highlight active experiment, and fix calling refresh() too often

--- ui/js/tests.js
+++ ui/js/tests.js
@@ -1,212 +1,233 @@
 describe("The view", function() {
-	var experiment
-	var experimentList
+  var experiment
+  var experimentList
 
-	beforeEach(function() {
-		experiment = { run: function() {}, url: ko.observable(""), state: ko.observable(""), view: function() {}, csvUrl: ko.observable(""), config: { pushes: ko.observable(1), concurrency: ko.observable(1)  } }
-		experimentList = { experiments: [], refresh: function(){} }
-		spyOn(experimentList, "refresh")
-		spyOn(experiment, "view")
-		spyOn(experiment, "run")
-		v = new pat.view(experimentList, experiment)
-		spyOn(v, "redirectTo").andReturn()
-		v.start()
-	})
+  beforeEach(function() {
+    experiment = { run: function() {}, url: ko.observable(""), state: ko.observable(""), view: function() {}, csvUrl: ko.observable(""), config: { pushes: ko.observable(1), concurrency: ko.observable(1)  } }
+    experimentList = { experiments: [], refreshNow: function(){} }
+    spyOn(experimentList, "refreshNow")
+    spyOn(experiment, "view")
+    spyOn(experiment, "run")
+    v = new pat.view(experimentList, experiment)
+    spyOn(v, "redirectTo").andReturn()
+    v.start()
+  })
 
-	describe("clicking start", function() {
-		it("runs the experiment", function() {
-			expect(experiment.run).toHaveBeenCalled()
-		})
-	})
+  describe("clicking start", function() {
+    it("runs the experiment", function() {
+      expect(experiment.run).toHaveBeenCalled()
+    })
+  })
 
-	describe("when the state of the experiment changes to running", function() {
-		beforeEach(function() { experiment.state("running") })
+  describe("when the state of the experiment changes to running", function() {
+    beforeEach(function() { experiment.state("running") })
 
-		it("sets canStart to false", function() {
-			expect(v.canStart()).toBe(false)
-		})
+    it("sets canStart to false", function() {
+      expect(v.canStart()).toBe(false)
+    })
 
-		it("sets canStop to true", function() {
-			expect(v.canStop()).toBe(true)
-		})
+    it("sets canStop to true", function() {
+      expect(v.canStop()).toBe(true)
+    })
 
-		it("sets noExperimentRunning to false", function() {
-			expect(v.noExperimentRunning()).toBe(false)
-		})
+    it("sets noExperimentRunning to false", function() {
+      expect(v.noExperimentRunning()).toBe(false)
+    })
 
-		it("refreshes the experiments list", function() {
-			expect(experimentList.refresh).toHaveBeenCalled()
-		})
-	})
+    it("refreshes the experiments list", function() {
+      expect(experimentList.refreshNow).toHaveBeenCalled()
+    })
+  })
 
-	describe("validation", function() {
-		it("prevents pushes being <= 0", function() {
-			v.numPushes(-1)
-			v.numConcurrent(1)
-			expect(v.numPushesHasError()).toBe(true)
-			expect(v.numConcurrentHasError()).toBe(false)
-			expect(v.formHasNoErrors()).toBe(false)
-		})
+  describe("validation", function() {
+    it("prevents pushes being <= 0", function() {
+      v.numPushes(-1)
+      v.numConcurrent(1)
+      expect(v.numPushesHasError()).toBe(true)
+      expect(v.numConcurrentHasError()).toBe(false)
+      expect(v.formHasNoErrors()).toBe(false)
+    })
 
-		it("prevents concurrency being <= 0", function() {
-			v.numConcurrent(-1)
-			v.numPushes(1)
-			expect(v.numPushesHasError()).toBe(false)
-			expect(v.numConcurrentHasError()).toBe(true)
-			expect(v.formHasNoErrors()).toBe(false)
-		})
-	})
+    it("prevents concurrency being <= 0", function() {
+      v.numConcurrent(-1)
+      v.numPushes(1)
+      expect(v.numPushesHasError()).toBe(false)
+      expect(v.numConcurrentHasError()).toBe(true)
+      expect(v.formHasNoErrors()).toBe(false)
+    })
+  })
 
-	describe("hash urls", function() {
-		it("does nothing if the hash is empty", function() {
-			v.onHashChange("#")
-			expect(experiment.view).not.toHaveBeenCalledWith()
-		})
+  describe("hash urls", function() {
+    it("does nothing if the hash is empty", function() {
+      v.onHashChange("#")
+      expect(experiment.view).not.toHaveBeenCalledWith()
+    })
 
-		it("views an experiment when the url hash changes", function() {
-			v.onHashChange("#foo.csv")
-			expect(experiment.view).toHaveBeenCalledWith("foo.csv")
-		})
-	})
+    it("views an experiment when the url hash changes", function() {
+      v.onHashChange("#foo.csv")
+      expect(experiment.view).toHaveBeenCalledWith("foo.csv")
+    })
+  })
 
-	describe("when the experiment has an associated CSV URL", function() {
-		beforeEach(function() { experiment.csvUrl("some-url.csv") })
+  describe("when the experiment has an associated CSV URL", function() {
+    beforeEach(function() { experiment.csvUrl("some-url.csv") })
 
-		it("sets canDownloadCsv to true", function() {
-			expect(v.canDownloadCsv()).toBe(true)
-		})
+    it("sets canDownloadCsv to true", function() {
+      expect(v.canDownloadCsv()).toBe(true)
+    })
 
-		describe("clicking downloadCsv", function() {
-			it("redirects to the csv URL", function() {
-				v.downloadCsv()
-				expect(v.redirectTo).toHaveBeenCalledWith("some-url.csv")
-			})
-		})
-	})
+    describe("clicking downloadCsv", function() {
+      it("redirects to the csv URL", function() {
+        v.downloadCsv()
+        expect(v.redirectTo).toHaveBeenCalledWith("some-url.csv")
+      })
+    })
+  })
 })
 
 describe("Throughput chart", function() {
-	var chart
+  var chart
 
-	beforeEach(function() {
-		chart = d3.custom.pats.throughput(d3.select("#target"))
-	})
+  beforeEach(function() {
+    chart = d3.custom.pats.throughput(d3.select("#target"))
+  })
 
-	it("should have a default width (300) and height (500)", function() {
-		expect(chart.width()).toBe(800)
-		expect(chart.height()).toBe(400)
-	})
+  it("should have a default width (300) and height (500)", function() {
+    expect(chart.width()).toBe(800)
+    expect(chart.height()).toBe(400)
+  })
 
-	it("should create a point for each element", function() {
-		chart([1, 2, 3])
-		expect(d3.selectAll('circle').size()).toBe(3)
-	})
+  it("should create a point for each element", function() {
+    chart([1, 2, 3])
+    expect(d3.selectAll('circle').size()).toBe(3)
+  })
 })
 
 describe("The experiment list", function() {
 
-	var self = this
-	var experiments
-	var list
+  var self = this
+  var experiments
+  var list
 
-	describe("refreshing", function() {
-		beforeEach(function() {
-			self.experiments = [ { name: "a" }, { name: "b" } ]
-			spyOn($, "get").andCallFake(function(url, callback) { console.log("ex", self.experiments); callback({ "Items": self.experiments }) })
-			list = pat.experimentList()
-		})
+  describe("refreshing", function() {
+    beforeEach(function() {
+      self.experiments = [ { name: "a", "Location": "notthisone" }, { name: "b", "Location": "/experiments/123" } ]
+      spyOn($, "get").andCallFake(function(url, callback) { console.log("ex", self.experiments); callback({ "Items": self.experiments }) })
+      list = pat.experimentList()
+    })
 
-		it("adds all the items to the experiments array", function() {
-			self.experiments = [1, 2, 3]
-			list.refresh()
-			expect(list.experiments()).toEqual(self.experiments)
-		})
+    it("adds all the items to the experiments array in reverse order", function() {
+      self.experiments = [1, 2, 3]
+      list.refresh()
+      expect(list.experiments()).toEqual(self.experiments.reverse())
+    })
 
-		it("refreshes on startup", function() {
-			expect(list.experiments()).toEqual(self.experiments)
-		})
-	})
+    it("refreshes on startup", function() {
+      expect(list.experiments()).toEqual(self.experiments.reverse())
+    })
+
+    describe("when an experimentChanged event fired", function() {
+      it("sets the active experiment in the list", function() {
+        $(document).trigger("experimentChanged", "/experiments/123")
+        active = list.experiments().filter(function(e) { return e.active() })
+        expect(active.length).toBe(1)
+        expect(active[0].name).toEqual("b")
+      })
+    })
+  })
 })
 
 describe("Running an experiment", function my() {
 
-	var replyUrl = "foo/bar/baz"
+  var replyUrl = "foo/bar/baz"
 
-	describe("Calling the endpoint", function() {
+  describe("Calling the endpoint", function() {
 
-		var pushes = 3
-		var concurrency = 5
-		var experiment
+    var pushes = 3
+    var concurrency = 5
+    var experiment
+    var listener = { onExperimentChanged: function() {} }
 
-		beforeEach(function() {
-			spyOn($, "post").andCallFake(function(url, data, callback) { callback({ "Location": replyUrl }) })
-			spyOn($, "get").andCallFake(function(url, callback) {  })
-			experiment = pat.experiment()
-			experiment.config.pushes(pushes)
-			experiment.config.concurrency(concurrency)
-			experiment.data([1,2,3])
-			experiment.run()
-		})
+    beforeEach(function() {
 
-		it("sends a POST to the /experiments/ endpoint", function() {
-			expect($.post).toHaveBeenCalledWith("/experiments/", jasmine.any(Object), jasmine.any(Function))
-		})
+      replyUrl = replyUrl + 1
 
-		it("sends the pushes and concurrency in the POST body", function() {
-			expect($.post.mostRecentCall.args[1].pushes).toBe(3)
-			expect($.post.mostRecentCall.args[1].concurrency).toBe(5)
-		})
+      spyOn($, "post").andCallFake(function(url, data, callback) { callback({ "Location": replyUrl }) })
+      spyOn($, "get").andCallFake(function(url, callback) {  })
+      spyOn(listener, "onExperimentChanged")
 
-		it("sends a GET to the tracking URL", function() {
-			expect($.get).toHaveBeenCalledWith(replyUrl, jasmine.any(Function))
-		})
+      $(document).on("experimentChanged", listener.onExperimentChanged)
 
-		it("clears any existing data", function() {
-			expect(experiment.data().length).toEqual(0)
-		})
-	})
+      experiment = pat.experiment()
+      experiment.config.pushes(pushes)
+      experiment.config.concurrency(concurrency)
+      experiment.data([1,2,3])
+      experiment.run()
+    })
 
-	describe("When results are returned", function() {
+    it("sends a POST to the /experiments/ endpoint", function() {
+      expect($.post).toHaveBeenCalledWith("/experiments/", jasmine.any(Object), jasmine.any(Function))
+    })
 
-		var refreshRate = 800
-		var csvUrl   = "foo/bar/baz.csv"
-		var experiment
-		var results
+    it("sends the pushes and concurrency in the POST body", function() {
+      expect($.post.mostRecentCall.args[1].pushes).toBe(3)
+      expect($.post.mostRecentCall.args[1].concurrency).toBe(5)
+    })
 
-		beforeEach(function() {
-			a = {"Type": 0, "name": "a"}
-			b = {"Type": 1, "name": "b"}
-			spyOn($, "post").andCallFake(function(url, data, callback) { callback({ "Location": replyUrl, "CsvLocation": csvUrl }) })
-			spyOn($, "get").andCallFake(function(url, callback) {
-				callback({ "Items": [a,b] })
-			})
+    it("sends a GET to the tracking URL", function() {
+      expect($.get).toHaveBeenCalledWith(replyUrl, jasmine.any(Function))
+    })
 
-			experiment = pat.experiment(refreshRate)
-			spyOn(experiment, "refresh").andCallThrough()
-			spyOn(experiment, "waitAndRefreshOnce") //mocked because jasmine.Clock was being painful
-			experiment.run()
-		})
+    it("clears any existing data", function() {
+      expect(experiment.data().length).toEqual(0)
+    })
 
-		it("updates with the latest data", function() {
-			expect(experiment.data()).toEqual([a])
-		})
+    it("triggers an experimentChanged event", function() {
+      expect(listener.onExperimentChanged).toHaveBeenCalledWith(jasmine.any(Object), replyUrl)
+    })
+  })
 
-		it("only includes data of type 0 (ResultSample)", function() {
-			expect(experiment.data()).not.toBe([a, b])
-		})
+  describe("When results are returned", function() {
 
-		it("refreshes the data at the refresh rate after data is returned", function() {
-			expect(experiment.waitAndRefreshOnce.callCount).toEqual(1)
-			$.get.mostRecentCall.args[1]({"Items": [{"Type": 0}]})
-			expect(experiment.waitAndRefreshOnce.callCount).toEqual(2)
-		})
+    var refreshRate = 800
+    var csvUrl   = "foo/bar/baz.csv"
+    var experiment
+    var results
 
-		it("updates the csv url", function() {
-			expect(experiment.csvUrl()).toBe(csvUrl)
-		})
+    beforeEach(function() {
+      a = {"Type": 0, "name": "a"}
+      b = {"Type": 1, "name": "b"}
+      spyOn($, "post").andCallFake(function(url, data, callback) { callback({ "Location": replyUrl, "CsvLocation": csvUrl }) })
+      spyOn($, "get").andCallFake(function(url, callback) {
+        callback({ "Items": [a,b] })
+      })
 
-		it("updates the state to 'running'", function() {
-			expect(experiment.state()).toBe("running")
-		})
-	})
+      experiment = pat.experiment(refreshRate)
+      spyOn(experiment, "refresh").andCallThrough()
+      spyOn(experiment, "waitAndRefreshOnce") //mocked because jasmine.Clock was being painful
+      experiment.run()
+    })
+
+    it("updates with the latest data", function() {
+      expect(experiment.data()).toEqual([a])
+    })
+
+    it("only includes data of type 0 (ResultSample)", function() {
+      expect(experiment.data()).not.toBe([a, b])
+    })
+
+    it("refreshes the data at the refresh rate after data is returned", function() {
+      expect(experiment.waitAndRefreshOnce.callCount).toEqual(1)
+      $.get.mostRecentCall.args[1]({"Items": [{"Type": 0}]})
+      expect(experiment.waitAndRefreshOnce.callCount).toEqual(2)
+    })
+
+    it("updates the csv url", function() {
+      expect(experiment.csvUrl()).toBe(csvUrl)
+    })
+
+    it("updates the state to 'running'", function() {
+      expect(experiment.state()).toBe("running")
+    })
+  })
 })
