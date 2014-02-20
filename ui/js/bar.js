a9d9c744289756fbfd34a011449eb6d21bf499a5
Simon Leung <leungs@us.ibm.com>
2014-02-20 10:03:59 -0800

    rebased against master for auto-merge

--- ui/js/bar.js
+++ ui/js/bar.js
@@ -1,7 +1,7 @@
 var barchart = function(el) {
   const d3_id = "d3_workload",
         d3_container = "d3_workload_container";
-        
+
   var margin = {top: 30, right: 30, bottom: 30, left: 30},
       d3Obj = d3.select(el),
       jqObj = $(el);
@@ -21,7 +21,7 @@ var barchart = function(el) {
   this.yAxis = d3.svg.axis()
     .scale(this.y)
     .orient("right")
-    .tickSize(-this.svgWidth + 30);  
+    .tickSize(-this.svgWidth + 30);
   this.zoom = d3.behavior.zoom()
     .x(this.x)
     .scaleExtent([1, 10])
@@ -65,21 +65,21 @@ var barchart = function(el) {
   this.outerBody.append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(0," + this.svgHeight + ")");
-  this.outerBody.append("g")  
+  this.outerBody.append("g")
     .attr("class", "y axis")
     .attr("transform", "translate(" + (this.svgWidth - 20) + ", 0)");
 
   this.barCon = chartBody.append("g")
     .attr("id", "chart_box")
-    .attr("transform", "translate(0,0)");  
+    .attr("transform", "translate(0,0)");
 
   this.svg.append("text")
-    .attr("x", -20)
-    .attr("y", -10)
-    .attr("transform", "rotate(-90)")
+    .attr("x", 20)
+    .attr("y", -this.svgWidth - 10)
+    .attr("transform", "rotate(90)")
     .text("Seconds");
   this.svg.append("text")
-    .attr("x", this.svgWidth - 60)
+    .attr("x", 20)
     .attr("y", this.svgHeight + 25)
     .text("Task #")
     .attr("text-anchor","middle");
@@ -87,16 +87,16 @@ var barchart = function(el) {
   var exports = function(data) {
     const second = 1000000000;
     var len = data.length;
-    var x = self.x.domain( [1, len, 1] ).range([self.barWidth + 1, len * (self.barWidth + 1)]);      
+    var x = self.x.domain( [1, len, 1] ).range([self.barWidth + 1, len * (self.barWidth + 1)]);
     var y = self.y.domain([d3.max(data, function(d) { return d.LastResult / second} ), 0] );
     var bars = self.barCon.selectAll("rect.bar").data(data);
     var labels = self.barCon.selectAll("text").data(data);
 
     panToViewable();
-    
+
     bars.transition()
       .attr("x", function(d, i) { return x(i) })
-      .attr("y", function(d) { return y(d.LastResult / second) } )  
+      .attr("y", function(d) { return y(d.LastResult / second) } )
       .attr("height", function(d) { return (self.svgHeight - y(d.LastResult / second)) } );
 
     labels.transition()
@@ -106,21 +106,21 @@ var barchart = function(el) {
     bars.enter()
       .append("rect")
         .attr("x", function(d, i) { return x(i) + 10 })
-        .attr("y", function(d) { return y(d.LastResult / second) } )  
+        .attr("y", function(d) { return y(d.LastResult / second) } )
         .attr("height", function(d) { return (self.svgHeight - y(d.LastResult / second)) } )
         .attr("width", self.barWidth)
         .attr("class", "bar")
-        .attr("data-shift", function(){ 
+        .attr("data-shift", function(){
             var bodyWidth = len * (self.barWidth + 1);
-            if (bodyWidth + getTranslateX(self.barCon) > self.drawBoxWidth) {    
+            if (bodyWidth + getTranslateX(self.barCon) > self.drawBoxWidth) {
               transformChart(self.barCon, self.drawBoxWidth - bodyWidth  - 25, 0);
               self.zoom = adjustZoomX(self.zoom, self.drawBoxWidth - bodyWidth  - 25, bodyWidth);
             }
         })
-      .transition()        
+      .transition()
         .duration(1000)
         .attr("x", function(d, i) { return x(i) })
-        .attr("y", function(d) { return y(d.LastResult / second) })     
+        .attr("y", function(d) { return y(d.LastResult / second) })
         .attr("height", function(d) { return (self.svgHeight - y(d.LastResult / second) ) } );
 
     bars.enter()
@@ -132,11 +132,11 @@ var barchart = function(el) {
       .transition()
         .attr("y", self.svgHeight + 3 );
 
-    self.outerBody.select(".x.axis").call(self.xAxis);    
+    self.outerBody.select(".x.axis").call(self.xAxis);
     self.outerBody.select(".y.axis").call(self.yAxis);
 
-    bars.exit().remove();   
-    labels.exit().remove();      
+    bars.exit().remove();
+    labels.exit().remove();
 
   } //end exports
 
@@ -146,19 +146,19 @@ var barchart = function(el) {
 
   function panToViewable() {
     var bodyWidth = getNodeWidth(self.barCon[0][0]);
-    var barsPan = getTranslateX(self.barCon);      
+    var barsPan = getTranslateX(self.barCon);
 
     if (bodyWidth + barsPan <= 0 && bodyWidth > 0) {
-      transformChart(self.barCon, ( 0 - bodyWidth) + self.drawBoxWidth / 3, 0);      
-      self.zoom = adjustZoomX(self.zoom, ((0 - bodyWidth) + self.drawBoxWidth / 3), bodyWidth);      
+      transformChart(self.barCon, ( 0 - bodyWidth) + self.drawBoxWidth / 3, 0);
+      self.zoom = adjustZoomX(self.zoom, ((0 - bodyWidth) + self.drawBoxWidth / 3), bodyWidth);
     } else if (barsPan > self.drawBoxWidth) {
-      transformChart(self.barCon, self.drawBoxWidth - (self.drawBoxWidth / 3), 0);      
-      self.zoom = adjustZoomX(self.zoom, self.drawBoxWidth - (self.drawBoxWidth / 3), bodyWidth);    
+      transformChart(self.barCon, self.drawBoxWidth - (self.drawBoxWidth / 3), 0);
+      self.zoom = adjustZoomX(self.zoom, self.drawBoxWidth - (self.drawBoxWidth / 3), bodyWidth);
     }
   }
 
-  function getTranslateX(node) {    
-    var splitted = node.attr("transform").split(",");  
+  function getTranslateX(node) {
+    var splitted = node.attr("transform").split(",");
     return parseInt(splitted [0].split("(")[1]);
   }
 
@@ -168,18 +168,18 @@ var barchart = function(el) {
 
   function transformChart(node, x, y) {
     node.transition()
-      .attr("transform", "translate(" + x + ", " + y + ")" );   
+      .attr("transform", "translate(" + x + ", " + y + ")" );
   }
 
   function adjustZoomX(zoom, x, range) {
-    zoom.x(d3.scale.linear().range([1, range]));                     
+    zoom.x(d3.scale.linear().range([1, range]));
     zoom.translate([x, 0]);
     return zoom;
   }
 
   return exports;
 
-}; //end barchart 
+}; //end barchart
 
 
 
