f435bc842f32c2d3a52d86590247c982491ed15e
Jonathan Berkhahn <jaberkha@us.ibm.com>
2014-02-25 16:09:50 -0800

    Implemented keeping track of 95th percentile

--- store/redis_test.go
+++ store/redis_test.go
@@ -80,20 +80,20 @@ var _ = Describe("Redis Store", func() {
 
 			writer := store.Writer("experiment-1")
 			write(writer, []*experiment.Sample{
-				&experiment.Sample{nil, 1, 2, 3, 4, 5, 6, nil, 7, 8, experiment.ResultSample},
-				&experiment.Sample{nil, 9, 8, 7, 6, 5, 4, errors.New("foo"), 3, 2, experiment.ResultSample},
+				&experiment.Sample{nil, 1, 2, 3, 4, 5, 6, nil, 7, 9, 8, experiment.ResultSample},
+				&experiment.Sample{nil, 9, 8, 7, 6, 5, 4, errors.New("foo"), 3, 1, 2, experiment.ResultSample},
 			})
 
 			writer = store.Writer("experiment-2")
 			write(writer, []*experiment.Sample{
-				&experiment.Sample{nil, 2, 2, 3, 4, 5, 6, nil, 7, 8, experiment.ResultSample},
+				&experiment.Sample{nil, 2, 2, 3, 4, 5, 6, nil, 7, 9, 8, experiment.ResultSample},
 			})
 
 			writer = store.Writer("experiment-3")
 			write(writer, []*experiment.Sample{
-				&experiment.Sample{nil, 1, 3, 3, 4, 5, 6, nil, 7, 8, experiment.ResultSample},
-				&experiment.Sample{nil, 2, 3, 3, 4, 5, 6, nil, 7, 8, experiment.ResultSample},
-				&experiment.Sample{nil, 9, 8, 7, 6, 5, 4, errors.New("foo"), 3, 2, experiment.ResultSample},
+				&experiment.Sample{nil, 1, 3, 3, 4, 5, 6, nil, 7, 9, 8, experiment.ResultSample},
+				&experiment.Sample{nil, 2, 3, 3, 4, 5, 6, nil, 7, 9, 8, experiment.ResultSample},
+				&experiment.Sample{nil, 9, 8, 7, 6, 5, 4, errors.New("foo"), 3, 1, 2, experiment.ResultSample},
 			})
 		})
 
