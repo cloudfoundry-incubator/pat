20fb17e68f1552ad78a2b13f41c703f34c325394
Julz Friedman <julz.friedman@uk.ibm.com>
2014-03-18 21:40:34 +0000

    Redis store

---
+++ store/redis.go
@@ -0,0 +1,78 @@
+package store
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/garyburd/redigo/redis"
+	"github.com/julz/pat/experiment"
+)
+
+const MAX_RESULTS = 10000
+
+type redisStore struct {
+	c redis.Conn
+}
+
+type redisExperiment struct {
+	redisStore *redisStore
+	guid       string
+}
+
+func NewRedisStore(host string, port int) (*redisStore, error) {
+	r, err := redis.Dial("tcp", fmt.Sprintf("%s:%d", host, port))
+	if err != nil {
+		return nil, err
+	}
+
+	return &redisStore{r}, nil
+}
+
+func (r *redisStore) LoadAll() ([]experiment.Experiment, error) {
+	c := r.c
+	members, err := redis.Strings(c.Do("LRANGE", "experiments", 0, MAX_RESULTS))
+	if err != nil {
+		return nil, err
+	}
+
+	experiments := make([]experiment.Experiment, len(members))
+	for i := 0; i < len(members); i++ {
+		experiments[i] = &redisExperiment{r, members[i]}
+	}
+
+	return experiments, nil
+}
+
+func (r *redisStore) Writer(guid string) func(samples <-chan *experiment.Sample) {
+	r.c.Do("RPUSH", "experiments", guid)
+	return func(ch <-chan *experiment.Sample) {
+		for sample := range ch {
+			push(r.c, guid, sample)
+		}
+	}
+}
+
+func push(c redis.Conn, guid string, sample *experiment.Sample) {
+	json, _ := json.Marshal(sample)
+	c.Do("RPUSH", "experiment."+guid, json)
+}
+
+func (r redisExperiment) GetData() ([]*experiment.Sample, error) {
+	members, err := redis.Strings(r.redisStore.c.Do("LRANGE", "experiment."+r.guid, 0, MAX_RESULTS))
+	if err != nil {
+		return nil, err
+	}
+
+	samples := make([]*experiment.Sample, len(members))
+	for i := 0; i < len(samples); i++ {
+		m := members[i]
+		b := []byte(m)
+		json.Unmarshal(b, &samples[i])
+	}
+
+	return samples, nil
+}
+
+func (r redisExperiment) GetGuid() string {
+	return r.guid
+}
---
+++ store/redis_test.go
@@ -0,0 +1,116 @@
+package store_test
+
+import (
+	"errors"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"time"
+
+	"github.com/julz/pat/experiment"
+	. "github.com/julz/pat/store"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+type store interface {
+	LoadAll() ([]experiment.Experiment, error)
+	Writer(name string) func(samples <-chan *experiment.Sample)
+}
+
+var _ = Describe("Redis Store", func() {
+	var (
+		store store
+		err   error
+	)
+
+	BeforeEach(func() {
+		_, filename, _, _ := runtime.Caller(0)
+		dir, _ := filepath.Abs(filepath.Dir(filename))
+		exec.Command("redis-cli", "-p", "63798", "shutdown").Run()
+		exec.Command("redis-server", dir+"/redis.conf").Run()
+		time.Sleep(500 * time.Millisecond) // yuck(jz)
+	})
+
+	Describe("Connecting", func() {
+		Context("When the host is wrong", func() {
+			It("Returns an error", func() {
+				_, err := NewRedisStore("fishfinger", 63798)
+				Ω(err).Should(HaveOccurred())
+			})
+		})
+
+		Context("When the port is wrong", func() {
+			It("Returns an error", func() {
+				_, err := NewRedisStore("localhost", 63799)
+				Ω(err).Should(HaveOccurred())
+			})
+		})
+
+		Context("When the host and port are correct", func() {
+			It("works", func() {
+				_, err := NewRedisStore("localhost", 63798)
+				Ω(err).ShouldNot(HaveOccurred())
+			})
+		})
+	})
+
+	Describe("Saving and Loading", func() {
+		AfterEach(func() {
+			exec.Command("redis-cli", "-p", "63798", "shutdown").Run()
+		})
+
+		BeforeEach(func() {
+			store, err = NewRedisStore("", 63798)
+			Ω(err).ShouldNot(HaveOccurred())
+
+			writer := store.Writer("experiment-1")
+			write(writer, []*experiment.Sample{
+				&experiment.Sample{nil, 1, 2, 3, 4, 5, 6, nil, 7, 8, experiment.ResultSample},
+				&experiment.Sample{nil, 9, 8, 7, 6, 5, 4, errors.New("foo"), 3, 2, experiment.ResultSample},
+			})
+
+			writer = store.Writer("experiment-2")
+			write(writer, []*experiment.Sample{
+				&experiment.Sample{nil, 2, 2, 3, 4, 5, 6, nil, 7, 8, experiment.ResultSample},
+			})
+
+			writer = store.Writer("experiment-3")
+			write(writer, []*experiment.Sample{
+				&experiment.Sample{nil, 1, 3, 3, 4, 5, 6, nil, 7, 8, experiment.ResultSample},
+				&experiment.Sample{nil, 2, 3, 3, 4, 5, 6, nil, 7, 8, experiment.ResultSample},
+				&experiment.Sample{nil, 9, 8, 7, 6, 5, 4, errors.New("foo"), 3, 2, experiment.ResultSample},
+			})
+		})
+
+		It("Round trips experiment list", func() {
+			experiments, err := store.LoadAll()
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(experiments).Should(HaveLen(3))
+		})
+
+		It("Round trips experiment guids", func() {
+			experiments, err := store.LoadAll()
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(experiments[0].GetGuid()).Should(Equal("experiment-1"))
+			Ω(experiments[1].GetGuid()).Should(Equal("experiment-2"))
+			Ω(experiments[2].GetGuid()).Should(Equal("experiment-3"))
+		})
+
+		It("Round trips samples", func() {
+			experiments, err := store.LoadAll()
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(data(experiments[0].GetData())).Should(HaveLen(2))
+			Ω(data(experiments[1].GetData())).Should(HaveLen(1))
+			Ω(data(experiments[2].GetData())).Should(HaveLen(3))
+		})
+
+		It("Round trips sample data", func() {
+			experiments, err := store.LoadAll()
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(data(experiments[0].GetData())[1].Total).Should(Equal(int64(7)))
+			Ω(data(experiments[1].GetData())[0].TotalErrors).Should(Equal(4))
+			Ω(data(experiments[2].GetData())[2].TotalWorkers).Should(Equal(5))
+		})
+	})
+})
